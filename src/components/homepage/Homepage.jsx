6b26881e088d1e327d4e8f3554a342f01a6949f6
Annaleigh Thomas <annaleighxvx@gmail.com>
2021-09-29 11:21:27 -0400

    homepage about set up

---
+++ src/components/homepage/Homepage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Homepage = () => {
+
+  return (
+    <div>
+      <section>
+        <h1>Annaleigh Hickman</h1>
+        <h4>Software Engineer</h4>
+      </section>
+    </div>
+  )
+
+}
+
+export default Homepage;
