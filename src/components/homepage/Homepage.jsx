fef918134a93d5c49d2835332403937ef859c182
Annaleigh Thomas <annaleighxvx@gmail.com>
2021-09-30 18:59:28 -0400

    added project section to homepage component

--- src/components/homepage/Homepage.jsx
+++ src/components/homepage/Homepage.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
+import ProjectsSection from './ProjectsSection'
 
 const Homepage = () => {
 
   return (
-    <div>
-      <section>
-        <h1>Annaleigh Hickman</h1>
-        <h4>Software Engineer</h4>
-      </section>
-    </div>
+    <>
+      <div>
+        <section>
+          <h1>Annaleigh Hickman</h1>
+          <h4>Software Engineer</h4>
+        </section>
+      </div>
+
+      <div>
+
+        <ProjectsSection />
+        
+      </div>
+    </>
   )
 
 }
